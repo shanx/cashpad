8effa9e1716d9732f9ebd693e143fc3710872d59
rplasman <rplasman@me.com>
2011-02-19 16:11:11 +0100

    Added ProductTableViewCell

---
+++ ipad/Classes/Customer.h
@@ -0,0 +1,31 @@
+//
+//  Customer.h
+//  CashRegister-iPad
+//
+//  Created by Rits Plasman on 19-02-11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@class Order;
+
+@interface Customer :  NSManagedObject  
+{
+}
+
+@property (nonatomic, retain) NSNumber * id;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSSet* orders;
+
+@end
+
+
+@interface Customer (CoreDataGeneratedAccessors)
+- (void)addOrdersObject:(Order *)value;
+- (void)removeOrdersObject:(Order *)value;
+- (void)addOrders:(NSSet *)value;
+- (void)removeOrders:(NSSet *)value;
+
+@end
+
---
+++ ipad/Classes/Customer.m
@@ -0,0 +1,19 @@
+// 
+//  Customer.m
+//  CashRegister-iPad
+//
+//  Created by Rits Plasman on 19-02-11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Customer.h"
+
+#import "Order.h"
+
+@implementation Customer 
+
+@dynamic id;
+@dynamic name;
+@dynamic orders;
+
+@end
