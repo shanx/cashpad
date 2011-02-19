a36e49d989774e796ebaf4ece9f39db90bb8fb42
Jan-Jaap Driessen <janjaapdriessen@gmail.com>
2011-02-19 16:57:00 +0100

    First stabs at API. And git.

--- web/src/cashpad/app.py
+++ web/src/cashpad/app.py
@@ -1,10 +1,60 @@
 import grok
+from zope.location.location import located
 
-from cashpad import resource
+class APILayer(grok.IRESTLayer):
+    grok.restskin('api')
 
-class Cashpad(grok.Application, grok.Container):
+class Users(grok.Container):
     pass
 
-class Index(grok.View):
-    def update(self):
-        resource.style.need()
+class App(grok.Application, grok.Container):
+    def __init__(self):
+        super(App, self).__init__()
+        self['user'] = Users()
+
+class AppTraverser(grok.Traverser):
+    grok.context(App)
+
+    def traverse(self, name):
+        if name == 'api':
+            grok.util.applySkin(self.request, APILayer, grok.IRESTSkinType)
+            return located(self.context, name)
+
+class Orders(grok.Container):
+    pass
+
+class User(grok.Container, grok.Model):
+    def __init__(self):
+        super(User, self).__init__()
+        self['order'] = Orders()
+
+# XXX PUT requests in grok don't work like you would expect them to?
+class UserTraverser(grok.Traverser):
+    grok.context(Users)
+    grok.layer(APILayer)
+
+    def traverse(self, name):
+        if self.request.method == 'PUT':
+            response = self.request.response
+            user = self.context.get(name)
+            if user is None:
+                user = self.context[name] = User()
+                response.setStatus('201')
+            else:
+                response.setStatus('204')
+            return located(self.context, name)
+
+class UserREST(grok.REST):
+    grok.context(Users)
+    grok.layer(APILayer)
+
+    def PUT(self):
+        # XXX We/grok should set the location here.
+        return ''
+
+class OrderREST(grok.REST):
+    grok.context(Orders)
+    grok.layer(APILayer)
+
+    def POST(self):
+        return "POST request, add something to container"
