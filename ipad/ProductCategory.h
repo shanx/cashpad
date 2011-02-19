5bff5a7a480867074f69aef15c1bd405037fe5b7
rplasman <rplasman@me.com>
2011-02-19 21:08:14 +0100

    Added ProductCategory model

---
+++ ipad/ProductCategory.h
@@ -0,0 +1,29 @@
+//
+//  ProductCategory.h
+//  CashRegister-iPad
+//
+//  Created by Rits Plasman on 19-02-11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@class Product;
+
+@interface ProductCategory :  NSManagedObject  
+{
+}
+
+@property (nonatomic, retain) NSSet* products;
+
+@end
+
+
+@interface ProductCategory (CoreDataGeneratedAccessors)
+- (void)addProductsObject:(Product *)value;
+- (void)removeProductsObject:(Product *)value;
+- (void)addProducts:(NSSet *)value;
+- (void)removeProducts:(NSSet *)value;
+
+@end
+
---
+++ ipad/ProductCategory.m
@@ -0,0 +1,17 @@
+// 
+//  ProductCategory.m
+//  CashRegister-iPad
+//
+//  Created by Rits Plasman on 19-02-11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ProductCategory.h"
+
+#import "Product.h"
+
+@implementation ProductCategory 
+
+@dynamic products;
+
+@end
