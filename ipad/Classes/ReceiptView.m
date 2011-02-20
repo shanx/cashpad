d4ccc36188df64fdef8ca049163a9590bd9680c5
Sergey A. Novitsky <serieux_chat@hotmail.com>
2011-02-20 01:04:29 +0100

    Update for the receipts view.

--- ipad/Classes/ReceiptView.m
+++ ipad/Classes/ReceiptView.m
@@ -9,43 +9,58 @@
 #import "ReceiptView.h"
 #import <QuartzCore/QuartzCore.h>
 #import "ReceiptTotalView.h"
+#import "ReceiptTitleView.h"
 
 @implementation ReceiptView
 
 @synthesize productTableView;
 @synthesize receiptTotalView;
+@synthesize receiptTitleView;
 
 #define TOTAL_VIEW_HEIGHT 100.0
+#define TITLE_VIEW_HEIGHT 50.0
 
 -(void)initCommon
 {
     self.layer.masksToBounds = YES;
     self.layer.borderColor = [UIColor grayColor].CGColor;
     self.layer.borderWidth = 1.0;
-    self.backgroundColor = [UIColor lightGrayColor];
+    // self.backgroundColor = [UIColor redColor];
     
     CGRect ownBounds = self.bounds;
-    CGRect totalViewFrame = CGRectMake(0.0, ownBounds.size.height - TOTAL_VIEW_HEIGHT, ownBounds.size.width, TOTAL_VIEW_HEIGHT);
     
-    receiptTotalView = [[UIView alloc] initWithFrame:totalViewFrame];
-    receiptTotalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+    CGRect titleViewFrame = CGRectMake(10.0, 10.0, 
+                                       ownBounds.size.width - 20.0, TITLE_VIEW_HEIGHT);
+    
+    
+    receiptTitleView = [[ReceiptTitleView alloc] initWithFrame:titleViewFrame];
 
-    receiptTotalView.layer.masksToBounds = YES;
-    receiptTotalView.layer.borderColor = [UIColor blueColor].CGColor;
-    receiptTotalView.layer.borderWidth = 1.0;
-    receiptTotalView.backgroundColor = [UIColor greenColor];
+    //receiptTitleView.layer.masksToBounds = YES;
+    //receiptTitleView.layer.borderColor = [UIColor grayColor].CGColor;
+    //receiptTitleView.layer.borderWidth = 1.0;
     
+    
+    [self addSubview:receiptTitleView];
+    
+    CGRect totalViewFrame = 
+        CGRectMake(0.0, ownBounds.size.height - TOTAL_VIEW_HEIGHT, 
+                                       ownBounds.size.width, TOTAL_VIEW_HEIGHT);
+    
+    receiptTotalView = [[ReceiptTotalView alloc] initWithFrame:totalViewFrame];
+    receiptTotalView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
     
     [self addSubview:receiptTotalView];
     
-    productTableView = [[UITableView alloc] initWithFrame:CGRectMake(0.0, 0.0, ownBounds.size.width, 
-                         ownBounds.size.height - TOTAL_VIEW_HEIGHT) style:UITableViewStylePlain];
+    productTableView = [[UITableView alloc] initWithFrame:
+                        CGRectMake(10.0, 10.0 + TITLE_VIEW_HEIGHT, ownBounds.size.width - 20.0, 
+                         ownBounds.size.height - TOTAL_VIEW_HEIGHT - TITLE_VIEW_HEIGHT - 10.0) 
+                                                    style:UITableViewStylePlain];
     
     productTableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleBottomMargin;
     
-    productTableView.layer.masksToBounds = YES;
-    productTableView.layer.borderColor = [UIColor redColor].CGColor;
-    productTableView.layer.borderWidth = 1.0;
+    //productTableView.layer.masksToBounds = YES;
+    //productTableView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    //productTableView.layer.borderWidth = 1.0;
     
     [self addSubview:productTableView];
 }
@@ -71,6 +86,7 @@
 {
     [receiptTotalView release];
     [productTableView release];
+    [receiptTitleView release];
     
     [super dealloc];
 }
